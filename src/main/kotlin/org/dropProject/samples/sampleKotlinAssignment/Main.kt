933a7650edd5ccbcc0785243be21c66dac2a24f1
Diogo Araújo <61624282+Diogo-a21905661@users.noreply.github.com>
2022-08-10 15:50:58 +0100

    Removed part of build.gradle and changed Main

--- src/main/kotlin/org/dropProject/samples/sampleKotlinAssignment/Main.kt
+++ src/main/kotlin/org/dropProject/samples/sampleKotlinAssignment/Main.kt
@@ -11,7 +11,7 @@ fun findMax(numbers: Array<Int>): Int {
     return max
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val numbers = arrayOf( 1, 3, 7, 4, 2)
     println("max = ${findMax(numbers)}")
 }
